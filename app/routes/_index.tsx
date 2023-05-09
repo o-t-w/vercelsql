43e77d428b0a0bb213a83e9816e204fc584e3fb4
Oliver Williams <oliverwilliams345@gmail.com>
2023-05-09 11:38:54 +0100

    new remix project

---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+     <h1>The future belongs to those who can smell it coming</h1>
+    </div>
+  );
+}
